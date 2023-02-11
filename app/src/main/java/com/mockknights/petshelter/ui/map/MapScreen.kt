89455ebb7cc6e5c1024928caa883a80afaaad091
Ismael Sabri <hisme14@gmail.com>
2023-02-11 17:52:08 +0100

    ModalScreen title and picture are similar to design - ModeratFamily has been created in Type.kt file - Extension functions drawTopLine and drawBottomLine have been added - ModalBox composable has been refactored

--- app/src/main/java/com/mockknights/petshelter/ui/map/MapScreen.kt
+++ app/src/main/java/com/mockknights/petshelter/ui/map/MapScreen.kt
@@ -1,29 +1,35 @@
 package com.mockknights.petshelter.ui.map
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 import com.mockknights.petshelter.R
 import com.mockknights.petshelter.domain.PetShelter
 import com.mockknights.petshelter.ui.components.createButton
+import com.mockknights.petshelter.ui.theme.moderatMediumTitle
+
 
 @OptIn(ExperimentalMaterialApi::class)
 @SuppressLint("RestrictedApi")
@@ -50,11 +56,13 @@ fun MapScreen(viewModel: MapViewModel = hiltViewModel()) {
             val modalShelterList = viewModel.modalShelterList.collectAsState().value
             var modalShelterName = ""
             var modalShelterNumber = ""
+            var modalShelterPhotoUrl = ""
             if(modalShelterList.isNotEmpty()) {
                 modalShelterName = modalShelterList[0].name
                 modalShelterNumber = modalShelterList[0].phoneNumber
+                modalShelterPhotoUrl = modalShelterList[0].photoURL
             }
-            ModalBox(modalShelterName, modalShelterNumber)
+            ModalBox(modalShelterName, modalShelterNumber, modalShelterPhotoUrl)
         }
     ) {
         Box(modifier = Modifier
@@ -64,12 +72,12 @@ fun MapScreen(viewModel: MapViewModel = hiltViewModel()) {
             if(!petShelter.value.isEmpty()) {
                 MyGoogleMaps(petShelter.value,
                     onPointClicked = {clickedShelterName ->
-                    viewModel.toggleModal()
+                        viewModel.toggleModal()
                         viewModel.setModalShelter(clickedShelterName)
-                    true // This way, the default behaviour of google maps is disabled (shows info window with title and snippet)
-                }, onMapClicked = {
-                    viewModel.collapse()
-                })
+                        true // This way, the default behaviour of google maps is disabled (shows info window with title and snippet)
+                    }, onMapClicked = {
+                        viewModel.collapse()
+                    })
                 Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Bottom) {
                     createButton(name = "ME", color = Color.Red) {
@@ -96,7 +104,7 @@ fun MyGoogleMaps(petShelter: List<PetShelter>, onPointClicked: (String) -> Boole
     val cameraPositionState = rememberCameraPositionState {
         position = CameraPosition.fromLatLngZoom(madrid, 5f)
     }
-   //3- PROPIEDADES DEL MAPA
+    //3- PROPIEDADES DEL MAPA
     val properties by remember {
         mutableStateOf(MapProperties(
             mapType = MapType.NORMAL,
@@ -132,58 +140,131 @@ fun MyGoogleMaps(petShelter: List<PetShelter>, onPointClicked: (String) -> Boole
 
 @Preview
 @Composable
-fun ModalBox(title: String = "Title", number: String = "NUMBER") {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(300.dp),
-
-    ) {
+fun ModalBox(title: String = "Title", phoneNumber: String = "NUMBER", photoUrl: String = "") {
+    val configuration = LocalConfiguration.current
+    val modalHeight = configuration.screenHeightDp.dp / 3
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(modalHeight),
+        ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-            // Title box
-            Box(
-                Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .border(BorderStroke(1.dp, Color.Gray))
-            ) {
-                Text(
-                    title,
-                    Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    fontSize = 40.sp,
-                    fontWeight = FontWeight.Normal
-                    )
-            }
-            Row(Modifier
-                .fillMaxSize(),
-                horizontalArrangement = Arrangement.Start,
-            ) {
-                val image: Painter = painterResource(id = R.drawable.ic_launcher_background)
-                Image(
-                    painter = image,
-                    contentDescription = "image",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                        .padding(10.dp))
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                        .padding(10.dp),
-                    verticalArrangement = Arrangement.SpaceEvenly,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    createButton(name = number, color = Color.Red, onClick = { /*TODO*/ })
-                    createButton(name = "GO", color = Color.Red, onClick = { /*TODO*/ })
-                }
-            }
+            // Title box: modifier passed as parameter to use columnScope
+            TitleBox(
+                title = title,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(2.6f))
+            // Row with the remaining elements
+            ImageAndButtonsRow(
+                photoUrl = photoUrl,
+                phoneNumber = phoneNumber,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(7.4f))
         }
     }
+}
+
+@Composable
+fun TitleBox(title: String, modifier: Modifier) {
+        Column(
+            modifier = modifier
+                .background(Color.LightGray)
+                .padding(top = 2.dp, bottom = 2.dp)
+                .drawBehind {
+                    drawTopLine(this, 5f)
+                    drawBottomLine(this, 5f)
+                }
+        ) {
+            Text(
+                title,
+                Modifier
+                    .fillMaxSize() // First expand it to max size
+                    .wrapContentHeight(), // Then, wrap content and align vertically the text
+                textAlign = TextAlign.Center,
+                color = Color.DarkGray,
+                style = MaterialTheme.typography.moderatMediumTitle,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+}
+
+@Composable
+fun ImageAndButtonsRow(photoUrl: String, phoneNumber: String, modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Start,
+    ) {
+        ImageColumn(
+            photoUrl = photoUrl,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(5.3f)
+                .padding(10.dp)
+        )
+        ButtonsColumn(
+            phoneNumber = phoneNumber,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(4.7f)
+                .padding(10.dp))
+    }
+}
+
+@Composable
+fun ImageColumn(photoUrl: String, modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = if (photoUrl.isNotEmpty()) "http://10.0.2.2:8080/$photoUrl" else R.drawable.ic_launcher_background,
+            contentDescription = "image",
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(20.dp))
+        )
+    }
+}
+
+@Composable
+fun ButtonsColumn(phoneNumber: String, modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        createButton(name = phoneNumber, color = Color.Red, onClick = { /*TODO*/ })
+        createButton(name = "GO", color = Color.Red, onClick = { /*TODO*/ })
+    }
+}
+
+// Extension functions
+fun drawTopLine(drawScope: DrawScope, width: Float) {
+    drawScope.drawLine(
+        Color.Black,
+        Offset(0f, 0f),
+        Offset(drawScope.size.width, 0f),
+        width
+    )
+}
+
+fun drawBottomLine(drawScope: DrawScope, width: Float) {
+    val y = drawScope.size.height
+    drawScope.drawLine(
+        Color.Black,
+        Offset(0f, y),
+        Offset(drawScope.size.width, y),
+        width
+    )
 }
 
--- app/src/main/java/com/mockknights/petshelter/ui/theme/Type.kt
+++ app/src/main/java/com/mockknights/petshelter/ui/theme/Type.kt
@@ -1,10 +1,16 @@
 package com.mockknights.petshelter.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mockknights.petshelter.R
+
+
+
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -13,16 +19,34 @@ val Typography = Typography(
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
 )
+
+// Create new family for font moderat
+val ModeratFamily = FontFamily(
+    Font(R.font.moderat_bold, FontWeight.Bold),
+    Font(R.font.moderat_medium, FontWeight.Medium),
+    Font(R.font.moderat_light, FontWeight.Light)
+)
+
+// Create extension styles that can be used as Material styles
+val Typography.moderatMediumTitle: TextStyle
+    @Composable
+    get() {
+        return  TextStyle(
+            fontFamily = ModeratFamily,
+            fontWeight = FontWeight.Medium,
+            fontSize = 24.sp,
+            letterSpacing = 15.sp
+        )
+    }
+
+val Typography.moderatLightTitle: TextStyle
+    @Composable
+    get() {
+        return  TextStyle(
+            fontFamily = ModeratFamily,
+            fontWeight = FontWeight.Light,
+            fontSize = 24.sp,
+            letterSpacing = 15.sp
+        )
+    }
