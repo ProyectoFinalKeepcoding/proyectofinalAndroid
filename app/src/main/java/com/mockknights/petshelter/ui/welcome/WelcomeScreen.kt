8ce0b14229c090f208f72aae7b16c19e9345b526
Aitor Iglesias <aiglesiaspubill@gmail.com>
2023-02-13 19:31:32 +0100

    Welcome fixed at button level

--- app/src/main/java/com/mockknights/petshelter/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/mockknights/petshelter/ui/welcome/WelcomeScreen.kt
@@ -1,5 +1,6 @@
 package com.mockknights.petshelter.ui.welcome
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
@@ -9,10 +10,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mockknights.petshelter.ui.components.CreateWelcomeButton
 import com.mockknights.petshelter.ui.theme.RedKiwoko
+import com.mockknights.petshelter.R
 
 @Preview(showSystemUi = true)
 @Composable
@@ -32,7 +36,7 @@ fun WelcomeScreen(/*viewModel: WelcomeViewModel = hiltViewModel(),*/ navigateToM
         Modifier
             .fillMaxWidth()
             .height(100.dp)) }
-
+    //SetBackground()
     Column(
         modifier = Modifier
             .fillMaxSize(),
@@ -44,17 +48,14 @@ fun WelcomeScreen(/*viewModel: WelcomeViewModel = hiltViewModel(),*/ navigateToM
                 .fillMaxSize()
                 .weight(1F)
                 .background(RedKiwoko), Alignment.Center
-        )
-
-        {
+        ) {
             CreateWelcomeButton(
                 name = "Puedo acoger a una mascota",
                 modifier = buttonModifier,
                 colorButton = Color.White,
                 colorText = RedKiwoko
-            )
-            {
-                navigateToMap()
+            ) {
+                navigateToLogin()
             }
         }
         Spacer(modifier = spacerModifier.background(RedKiwoko))
@@ -72,8 +73,19 @@ fun WelcomeScreen(/*viewModel: WelcomeViewModel = hiltViewModel(),*/ navigateToM
                 colorButton = RedKiwoko,
                 colorText = Color.White
             ) {
-                navigateToLogin()
+                navigateToMap()
             }
         }
     }
+}
+
+
+@Composable
+fun SetBackground() {
+    val imageModifier = Modifier.fillMaxSize()
+    Image(
+        painter = painterResource(id = R.drawable.welcome),
+        contentDescription = "Fondo pantalla",
+        contentScale = ContentScale.Crop,
+        modifier = imageModifier)
 }
