a903e272cdc76aa00a0cd20ad1ccc86c56dea6a9
Ismael Sabri <hisme14@gmail.com>
2023-02-26 20:20:44 +0100

    Added lottie dog to welcome screen

--- app/src/main/java/com/mockknights/petshelter/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/com/mockknights/petshelter/ui/welcome/WelcomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -12,6 +13,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
 import com.mockknights.petshelter.ui.components.CreateWelcomeButton
 import com.mockknights.petshelter.ui.theme.RedKiwoko
 import com.mockknights.petshelter.R
@@ -59,17 +61,32 @@ fun WelcomeScreen( navigateToMap: () -> (Unit) = {}, navigateToLogin: () -> (Uni
                 Spacer(
                     modifier = Modifier
                         .fillMaxSize()
-                        .weight(3.8f)
+                        .weight(2.4f)
                 )
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .weight(3.4f)
+                        .weight(4.8f),
+                    verticalArrangement = Arrangement.Top
                 ) {
+                    // Lottie file dog walking (not showing)
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(3f)
+                            .wrapContentHeight(),
+                        horizontalArrangement = Arrangement.Start
+                    ) {
+                        WalkingDog(
+                            modifier = Modifier
+                                .fillMaxSize()
+                        )
+                    }
                     CreateWelcomeButton(
                         name = "He encontrado una mascota",
                         modifier = Modifier
                             .fillMaxWidth()
+                            .weight(3f)
                             .wrapContentHeight()
                             .padding(horizontal = 16.toDp().dp),
                         colorButton = RedKiwoko,
@@ -77,6 +94,11 @@ fun WelcomeScreen( navigateToMap: () -> (Unit) = {}, navigateToLogin: () -> (Uni
                     ) {
                         navigateToMap()
                     }
+                    Spacer(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(4f)
+                    )
                 }
 
             }
@@ -92,4 +114,24 @@ fun BackgroundImage() {
         contentDescription = "Fondo pantalla",
         contentScale = ContentScale.FillBounds,
         modifier = Modifier.fillMaxSize())
+}
+
+@Preview // No preview for lottie files yet
+@Composable
+fun WalkingDog(modifier: Modifier = Modifier) {
+    val compositeResult: LottieCompositionResult = rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.dog)
+    )
+    val progress by animateLottieCompositionAsState(
+        compositeResult.value,
+        isPlaying = true,
+        iterations = LottieConstants.IterateForever
+    )
+    LottieAnimation(
+        composition = compositeResult.value,
+        progress = { progress },
+        alignment = Alignment.TopStart,
+        contentScale = ContentScale.FillHeight,
+        modifier = modifier
+    )
 }
